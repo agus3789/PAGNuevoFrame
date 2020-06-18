3d9cd1a964691ea0c7a10f65ca76af1b5b3cd829
unknown <appollonioagustin@gmail.com>
2020-06-18 03:15:05 -0300

    “PAGNuevoFramework”

---
+++ js/func.js
@@ -0,0 +1,182 @@
+function check()
+{
+    var q1 = document.quiz.q1.value;
+    var q2 = document.quiz.q2.value;
+    var q3 = document.quiz.q3.value;
+    var q4 = document.quiz.q4.value;
+    var porcentaje;
+    var cont=0;
+   
+
+    
+    if(q1 == "SI")
+    {
+       cont++;
+    }
+    if(q2 == "NO")
+    {
+       cont++;
+    }
+    if(q3 == "SI")
+    {
+       cont++;
+    }
+    if(q4 == "NO")
+    {
+       cont++;
+    }
+    
+    
+    porcentaje = cont*100/4;
+    porcentaje = parseInt(porcentaje);
+
+  
+    return alert("Tuviste un " + porcentaje + "% de aciertos");
+
+}
+
+function check2()
+{
+    var q1 = document.quiz.q5.value;
+    var q2 = document.quiz.q6.value;
+    var q3 = document.quiz.q7.value;
+    var q4 = document.quiz.q8.value;
+    var porcentaje;
+    var cont=0;
+   
+
+    
+    if(q1 == "SI")
+    {
+       cont++;
+    }
+    if(q2 == "SI")
+    {
+       cont++;
+    }
+    if(q3 == "NO")
+    {
+       cont++;
+    }
+    if(q4 == "NO")
+    {
+       cont++;
+    }
+    
+    
+    porcentaje = cont*100/4;
+    porcentaje = parseInt(porcentaje);
+
+  
+    return alert("Tuviste un " + porcentaje + "% de aciertos");
+
+}
+function check3()
+{
+    var q1 = document.quiz.q9.value;
+    var q2 = document.quiz.q10.value;
+    var q3 = document.quiz.q11.value;
+    var q4 = document.quiz.q12.value;
+    var porcentaje;
+    var cont=0;
+   
+
+    
+    if(q1 == "SI")
+    {
+       cont++;
+    }
+    if(q2 == "SI")
+    {
+       cont++;
+    }
+    if(q3 == "SI")
+    {
+       cont++;
+    }
+    if(q4 == "SI")
+    {
+       cont++;
+    }
+    
+    
+    porcentaje = cont*100/4;
+    porcentaje = parseInt(porcentaje);
+
+  
+    return alert("Tuviste un " + porcentaje + "% de aciertos");
+
+}
+function check4()
+{
+    var q1 = document.quiz.q13.value;
+    var q2 = document.quiz.q14.value;
+    var q3 = document.quiz.q15.value;
+    var q4 = document.quiz.q16.value;
+    var porcentaje;
+    var cont=0;
+   
+
+    
+    if(q1 == "SI")
+    {
+       cont++;
+    }
+    if(q2 == "SI")
+    {
+       cont++;
+    }
+    if(q3 == "SI")
+    {
+       cont++;
+    }
+    if(q4 == "NO")
+    {
+       cont++;
+    }
+    
+    
+    porcentaje = cont*100/4;
+    porcentaje = parseInt(porcentaje);
+
+  
+    return alert("Tuviste un " + porcentaje + "% de aciertos");
+
+}
+function check5()
+{
+    var q1 = document.quiz.q17.value;
+    var q2 = document.quiz.q18.value;
+    var q3 = document.quiz.q19.value;
+    var q4 = document.quiz.q20.value;
+    var porcentaje;
+    var cont=0;
+   
+
+    
+    if(q1 == "SI")
+    {
+       cont++;
+    }
+    if(q2 == "NO")
+    {
+       cont++;
+    }
+    if(q3 == "Will they be eating?")
+    {
+       cont++;
+    }
+    if(q4 == "Jose will be watching the news when you call")
+    {
+       cont++;
+    }
+    
+    
+    porcentaje = cont*100/4;
+    porcentaje = parseInt(porcentaje);
+
+  
+    return alert("Tuviste un " + porcentaje + "% de aciertos");
+
+}
+
